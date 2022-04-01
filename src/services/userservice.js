d6487049bf224fed21d9141d68355fd932854651
nahid6773 <nana.rezayi3@gmail.com>
2022-04-01 23:25:37 +0430

    message

--- src/services/userservice.js
+++ src/services/userservice.js
@@ -8,3 +8,10 @@ export const userregister=user=>{
 export const userlogin=user=>{
     return axiosMethods.post(`${config.fibatoapi}/api/login`,JSON.stringify(user));
 }
+
+// ببین چند تا ارور دیگه داری دونه دونه توی گوگل سرچشون کن برطرفشون کن بعد چند تا دستور گیت به ترتیب میزنی وقتی که کامل باگ هات و فیکس کردی
+// git add .
+// git commit -m "message"
+// git push
+// بعد دوباره برو توی اون سایته اگه مشکلی نداشته باشه deploy میشه اگه مشکل داشته باشه به
+//الان ارورش چیه اونجا نوشته بخون سرچ کن
